742004fd71f785d58db18092555fc2d82618734c
MRxwr <nasserhatab@gmail.com>
2024-03-25 15:34:53 +0300

    1st push

---
+++ admin/includes/checksouthead.php
@@ -0,0 +1,21 @@
+<?php
+require ("config.php");
+require ("translate.php");
+require ("functions.php");
+if ( isset ( $_COOKIE[$cookieSession."A"] ) ){
+	session_start ();
+	$svdva = $_COOKIE[$cookieSession."A"];
+	if ( $user = selectDB("employees","`keepMeAlive` LIKE '%".$svdva."%'") ){
+		$userID = $user[0]["id"];
+		$email = $user[0]["email"];
+		$username = $user[0]["fullName"];
+		$userType = $user[0]["empType"];
+		$academiesList = ( is_array($user[0]["academyId"]) && empty($user[0]["academyId"]) ) ? array() : json_decode($user[0]["academyId"],true);
+		$_SESSION[$cookieSession."A"] = $email;	
+	}else{
+		header("Location: logout.php");die();
+	}
+}elseif( !isset ( $_COOKIE[$cookieSession."A"] ) ){
+	header("Location: login.php");die();
+}
+?>
