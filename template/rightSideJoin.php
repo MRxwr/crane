ed6744204c715e507e1bd0aae82ad0956015f650
MRxwr <nasserhatab@gmail.com>
2024-05-03 07:37:13 +0300

    push

---
+++ template/rightSideJoin.php
@@ -0,0 +1,48 @@
+<div class="rightContent sidesJoin">
+    <div class="rightContentJoinBg"></div>
+    <div class="rightContentDivs">
+    <div class="row m-0">
+        <div class="col-md-6 p-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Join Crane</h1>
+                    <h5 style="font-weight:400"><p>Are you a driver ! join Crane now !<p>Our goal to expand Crane family.</p><p>Fill the details and we will contact you shortly.</p>
+                    </h5>
+                    <img src="img/join-our-family.jpg" class="rounded" style="width:65%">
+                </div>
+                <div class="col-xl-6 p-3">
+                    <button type="button" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill:white;height:25px;width:25px"><path d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/></svg> Google Play</button>
+                </div>
+                <div class="col-xl-6 p-3">
+                    <button type="button" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" style="fill:white;height:25px;width:25px" viewBox="0 0 384 512"><path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z"/></svg> AppStore</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 p-5">
+            <h1>Send us a message</h1>
+            <h5 style="font-weight:400">Don't worry we always reply to our customers.</h5>
+            <div class="row m-0 pt-3" style="font-size:20px">
+                <div class="col-md-6 p-1">Name</div>
+                <div class="col-md-6 p-1">Email</div>
+                <div class="col-md-6 p-1"><input class='form-control' type='text' ></div>
+                <div class="col-md-6 p-1"><input class='form-control' type='text' ></div>
+                <div class="col-md-6 p-1">Number</div>
+                <div class="col-md-6 p-1">Service Type</div>
+                <div class="col-md-6 p-1"><input class='form-control' type='text' ></div>
+                <div class="col-md-6 p-1">
+                    <select class='form-control' >
+                        <option>Towing Truck</option>
+                        <option>Towing Truck</option>
+                        <option>Towing Truck</option>
+                        <option>Towing Truck</option>
+                        <option>Towing Truck</option>
+                    </select>
+                </div>
+                <div class="col-12 p-1">Your Message</div>
+                <div class="col-12 p-1"><textarea class='form-control' style='height:200px'></textarea></div>
+                <div class="col-12 p-1"><button class='btn btn-primary w-100'>Send It ! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill:black;height:20px;width:20px"><path d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2v82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9V380.8c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg></button></div>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
