5da00834ef7f5b4ab67ae040d7987032bef7e93c
MRxwr <nasserhatab@gmail.com>
2024-05-03 03:37:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
-<?php
-if( isset($_GET["Result"]) ){
-	if( $_GET["Result"] == "CANCELED" || $_GET["Result"] == "ERROR" ){
-		$_GET["v"] = "Fail";
-	}elseif( $_GET["Result"] == "CAPTURED" ){
-		sleep(5);
-		$_GET["v"] = "Success";
-	}
-}
-// Set headers before any output
-header('Content-Type: text/html; charset=utf-8');
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-require("template/header.php");
-require("template/navbar.php");
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-// get viewed page from pages folder \\
-if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
-	require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
-}else{
-	require_once("views/bladeHome.php");
-}
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
 
-require("template/footer.php");
-?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
