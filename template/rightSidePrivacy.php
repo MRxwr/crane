cd6cd4a2beb54056e687105112e4a24015a9f674
MRxwr <nasserhatab@gmail.com>
2024-05-03 07:27:12 +0300

    Create rightSidePrivacy.php

---
+++ template/rightSidePrivacy.php
@@ -0,0 +1,28 @@
+<div class="rightContent sides">
+    <div class="rightContentBg"></div>
+    <div class="rightContentDivs">
+    <div class="row m-0">
+        <div class="col-md-12 p-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Privacy & Policy</h1>
+                    <h5 style="font-weight:400">Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been
+                        the industry
+                        's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of
+                        type and scrambled it to make a type specimen
+                        book. It has survived not only five centuries,
+                    </h5>
+                </div>
+                <div class="col-xl-6 p-3">
+                    <button type="button" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill:white;height:25px;width:25px"><path d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/></svg> Google Play</button>
+                </div>
+                <div class="col-xl-6 p-3">
+                    <button type="button" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" style="fill:white;height:25px;width:25px" viewBox="0 0 384 512"><path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z"/></svg> AppStore</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
