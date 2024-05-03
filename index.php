11577c26bcefe8cf1644f25ac5c140380453be08
MRxwr <nasserhatab@gmail.com>
2024-05-03 08:21:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -83,10 +83,10 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	
 	<script>
 		$(function() {
 			$(document).ready(function() {
@@ -94,6 +94,7 @@
 					$('#rightSide').fadeOut(function() {
 						$.get('template/rightSideHow.php', function(data) {
 							$('#rightSide').html(data).fadeIn();
+							$('#fullscreenModal').modal('hide');
 						});
 					});
 				});
@@ -101,6 +102,7 @@
 					$('#rightSide').fadeOut(function() {
 						$.get('template/rightSideHome.php', function(data) {
 							$('#rightSide').html(data).fadeIn();
+							$('#fullscreenModal').modal('hide');
 						});
 					});
 				});
@@ -108,6 +110,7 @@
 					$('#rightSide').fadeOut(function() {
 						$.get('template/rightSideServices.php', function(data) {
 							$('#rightSide').html(data).fadeIn();
+							$('#fullscreenModal').modal('hide');
 						});
 					});
 				});
@@ -115,6 +118,7 @@
 					$('#rightSide').fadeOut(function() {
 						$.get('template/rightSideContact.php', function(data) {
 							$('#rightSide').html(data).fadeIn();
+							$('#fullscreenModal').modal('hide');
 						});
 					});
 				});
@@ -122,6 +126,7 @@
 					$('#rightSide').fadeOut(function() {
 						$.get('template/rightSidePrivacy.php', function(data) {
 							$('#rightSide').html(data).fadeIn();
+							$('#fullscreenModal').modal('hide');
 						});
 					});
 				});
@@ -129,6 +134,7 @@
 					$('#rightSide').fadeOut(function() {
 						$.get('template/rightSideJoin.php', function(data) {
 							$('#rightSide').html(data).fadeIn();
+							$('#fullscreenModal').modal('hide');
 						});
 					});
 				});
