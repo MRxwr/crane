2e7937c31c2306e3279259e43c4697fa0a31d153
MRxwr <nasserhatab@gmail.com>
2024-05-03 07:25:54 +0300

    push

--- index.php
+++ index.php
@@ -8,75 +8,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://use.fontawesome.com/245c9398b0.js"></script>
-
     <title>Crane.</title>
-
-	<Style>
-		.sides{
-			/*height:100vh;*/
-			border-radius: 20px;
-			background-color: #000000;
-			color: white;
-			z-index:1;
-		}
-		.craneDot{
-			font-size:50px;
-			color: #038dff;
-		}
-		.craneDotBig{
-			font-size:50px;
-			color: #038dff;
-		}
-		.craneBg{
-			font-size:200px;
-			color: #ffffff;
-		}
-		ul {
-			list-style: none;
-			padding: 0;
-		}
-		li {
-			padding: 10px;
-			font-size: 20px;
-
-		}
-		li:hover,li.selected {
-			text-decoration: underline;
-			color: #038dff; /* Change the color to blue */
-			cursor: pointer;
-		}
-		.joinBtn{
-			background-color: #038dff;
-			color: #FFFFFF;
-			font-size: 25px;
-			font-weight: 500;
-			width: 100%;
-		}
-		.lang{
-			border: 1px white solid;
-			font-size: 18px;
-			padding: 10px
-		}
-		.socialMediaIcon{
-			fill: white;
-    		height: 30px;
-		}
-		.rightContentBg {
-			position: absolute;
-			bottom: 17px;
-			left: 13px;
-			width: 95%;
-			height: 45%;
-			background-image: url(img/bg_bg.png);
-			background-size: cover;
-			background-position: center;
-			z-index: 0;
-		}
-		.rightContentDivs {
-			padding: 20px; /* Add padding to keep text away from edges */
-			z-index: 1; /* Bring the text content to the front */
-		}
-	</style>
+	<style><?php echo require("css/style.css") ?></style>
   </head>
   <body>
     <div class="content-fluid" style="height:100vh">
@@ -86,7 +19,8 @@
 					<div class="col-md-3 d-flex p-2">
 							<?php require("template/leftSide.php");?>
 					</div>
-					<div class="col-md-9 d-flex p-2">
+					<div class="col-md-9 d-flex p-2" id="rightSide">
+					<div id="result" style="display: none;">Loading...</div>
 							<?php require("template/rightSideHome.php"); ?>
 					</div>
 				</div>
@@ -99,5 +33,52 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script>
+		$(function() {
+			$('#how').on('click', function() {
+				$('#rightSide').fadeOut(function() {
+					$.get('template/rightSideHow.php', function(data) {
+						$('#rightSide').html(data).fadeIn();
+					});
+				});
+			});
+			$('#home').on('click', function() {
+				$('#rightSide').fadeOut(function() {
+					$.get('template/rightSideHome.php', function(data) {
+						$('#rightSide').html(data).fadeIn();
+					});
+				});
+			});
+			$('#services').on('click', function() {
+				$('#rightSide').fadeOut(function() {
+					$.get('template/rightSideServices.php', function(data) {
+						$('#rightSide').html(data).fadeIn();
+					});
+				});
+			});
+			$('#contact').on('click', function() {
+				$('#rightSide').fadeOut(function() {
+					$.get('template/rightSideContact.php', function(data) {
+						$('#rightSide').html(data).fadeIn();
+					});
+				});
+			});
+			$('#privacy').on('click', function() {
+				$('#rightSide').fadeOut(function() {
+					$.get('template/rightSidePrivacy.php', function(data) {
+						$('#rightSide').html(data).fadeIn();
+					});
+				});
+			});
+			$('#join').on('click', function() {
+				$('#rightSide').fadeOut(function() {
+					$.get('template/rightSideJoin.php', function(data) {
+						$('#rightSide').html(data).fadeIn();
+					});
+				});
+			});
+		});
+	</script>
   </body>
 </html>
---
+++ template/rightSideServices.php
@@ -0,0 +1,50 @@
+<div class="rightContent sides">
+    <div class="rightContentBg"></div>
+    <div class="rightContentDivs">
+    <div class="row m-0">
+        <div class="col-md-6 p-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Services</h1>
+                    <h5 style="font-weight:400">Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been
+                        the industry
+                        's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of
+                        type and scrambled it to make a type specimen
+                        book. It has survived not only five centuries,
+                    </h5>
+                </div>
+                <div class="col-xl-6 p-3">
+                    <button type="button" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill:white;height:25px;width:25px"><path d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/></svg> Google Play</button>
+                </div>
+                <div class="col-xl-6 p-3">
+                    <button type="button" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" style="fill:white;height:25px;width:25px" viewBox="0 0 384 512"><path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z"/></svg> AppStore</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 p-5">
+            <div class="row" style="font-size:20px">
+                <div class="col-md-6 p-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="fill:white;height:25px;width:25px"><path d="M368.6 96l76.8 96H288V96h80.6zM224 80V192H64c-17.7 0-32 14.3-32 32v64c-17.7 0-32 14.3-32 32s14.3 32 32 32H65.1c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16h66.3c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32V224c0-17.7-14.3-32-32-32H527.4L418.6 56c-12.1-15.2-30.5-24-50-24H272c-26.5 0-48 21.5-48 48zm0 288a48 48 0 1 1 -96 0 48 48 0 1 1 96 0zm288 0a48 48 0 1 1 -96 0 48 48 0 1 1 96 0z"/></svg> <label class="pr-3">Car Towing</label>
+                </div>
+                <div class="col-md-6 p-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="fill:white;height:25px;width:25px"><path d="M368.6 96l76.8 96H288V96h80.6zM224 80V192H64c-17.7 0-32 14.3-32 32v64c-17.7 0-32 14.3-32 32s14.3 32 32 32H65.1c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16h66.3c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32V224c0-17.7-14.3-32-32-32H527.4L418.6 56c-12.1-15.2-30.5-24-50-24H272c-26.5 0-48 21.5-48 48zm0 288a48 48 0 1 1 -96 0 48 48 0 1 1 96 0zm288 0a48 48 0 1 1 -96 0 48 48 0 1 1 96 0z"/></svg> <label class="pr-3">Car Towing</label>
+                </div>
+                <div class="col-md-6 p-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="fill:white;height:25px;width:25px"><path d="M368.6 96l76.8 96H288V96h80.6zM224 80V192H64c-17.7 0-32 14.3-32 32v64c-17.7 0-32 14.3-32 32s14.3 32 32 32H65.1c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16h66.3c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32V224c0-17.7-14.3-32-32-32H527.4L418.6 56c-12.1-15.2-30.5-24-50-24H272c-26.5 0-48 21.5-48 48zm0 288a48 48 0 1 1 -96 0 48 48 0 1 1 96 0zm288 0a48 48 0 1 1 -96 0 48 48 0 1 1 96 0z"/></svg> <label class="pr-3">Car Towing</label>
+                </div>
+                <div class="col-md-6 p-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="fill:white;height:25px;width:25px"><path d="M368.6 96l76.8 96H288V96h80.6zM224 80V192H64c-17.7 0-32 14.3-32 32v64c-17.7 0-32 14.3-32 32s14.3 32 32 32H65.1c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16h66.3c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32V224c0-17.7-14.3-32-32-32H527.4L418.6 56c-12.1-15.2-30.5-24-50-24H272c-26.5 0-48 21.5-48 48zm0 288a48 48 0 1 1 -96 0 48 48 0 1 1 96 0zm288 0a48 48 0 1 1 -96 0 48 48 0 1 1 96 0z"/></svg> <label class="pr-3">Car Towing</label>
+                </div>
+                <div class="col-md-6 p-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="fill:white;height:25px;width:25px"><path d="M368.6 96l76.8 96H288V96h80.6zM224 80V192H64c-17.7 0-32 14.3-32 32v64c-17.7 0-32 14.3-32 32s14.3 32 32 32H65.1c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16h66.3c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32V224c0-17.7-14.3-32-32-32H527.4L418.6 56c-12.1-15.2-30.5-24-50-24H272c-26.5 0-48 21.5-48 48zm0 288a48 48 0 1 1 -96 0 48 48 0 1 1 96 0zm288 0a48 48 0 1 1 -96 0 48 48 0 1 1 96 0z"/></svg> <label class="pr-3">Car Towing</label>
+                </div>
+                <div class="col-md-6 p-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="fill:white;height:25px;width:25px"><path d="M368.6 96l76.8 96H288V96h80.6zM224 80V192H64c-17.7 0-32 14.3-32 32v64c-17.7 0-32 14.3-32 32s14.3 32 32 32H65.1c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16h66.3c-.7 5.2-1.1 10.6-1.1 16c0 61.9 50.1 112 112 112s112-50.1 112-112c0-5.4-.4-10.8-1.1-16H608c17.7 0 32-14.3 32-32s-14.3-32-32-32V224c0-17.7-14.3-32-32-32H527.4L418.6 56c-12.1-15.2-30.5-24-50-24H272c-26.5 0-48 21.5-48 48zm0 288a48 48 0 1 1 -96 0 48 48 0 1 1 96 0zm288 0a48 48 0 1 1 -96 0 48 48 0 1 1 96 0z"/></svg> <label class="pr-3">Car Towing</label>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
