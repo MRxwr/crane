4974b494091362bd3804cb4dd0dc2f8ef725e306
MRxwr <nasserhatab@gmail.com>
2024-05-19 00:08:16 +0300

    Update index.php

--- template/rightSide/index.php
+++ template/rightSide/index.php
@@ -1,7 +1,7 @@
 <?php 
-require_once("admin/includes/config.php");
-require_once("admin/includes/functions.php");
-require_once("admin/includes/translate.php");
+require_once("../../admin/includes/config.php");
+require_once("../../admin/includes/functions.php");
+require_once("../../admin/includes/translate.php");
 
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("template/rightSide","blade{$_GET["v"]}.php") ){
