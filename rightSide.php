99a9fbb399be0ab825faa72f4b2b26c1a78d1339
MRxwr <nasserhatab@gmail.com>
2024-05-19 00:46:48 +0300

    home

--- rightSide.php
+++ rightSide.php
@@ -3,6 +3,8 @@ require_once("admin/includes/config.php");
 require_once("admin/includes/functions.php");
 require_once("admin/includes/translate.php");
 
+$settings = selectDB("settings","`id` = '1'");
+
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
 	require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
