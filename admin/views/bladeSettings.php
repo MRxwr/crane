5a8f849fa7dae2e12eb5999b0e7a4f74eb1e9a17
MRxwr <nasserhatab@gmail.com>
2024-05-19 00:36:03 +0300

    Update bladeSettings.php

--- admin/views/bladeSettings.php
+++ admin/views/bladeSettings.php
@@ -63,7 +63,7 @@ if( $settings = selectDB("settings","`id` = '1'")){
 		</div>
 	</div>
 
-	<!-- system main IBAN -->
+	<!-- system main Email -->
 	<div class="col-md-6">
 		<div class="panel panel-default card-view">
 			<div class="panel-heading">
@@ -83,6 +83,34 @@ if( $settings = selectDB("settings","`id` = '1'")){
 	</div>
 
 	<!-- system en terms -->
+<?php
+$pages = ["Home","How","Services","Contact","Join","Policy"];
+$lang = ["English","Arabic"];
+$dbCol = ["en","ar"];
+for( $i = 0; $i < sizeof($pages); $i++ ){
+	for( $y = 0; $y < 2; $y++ ){
+		$dbPlusLang = $dbCol[$y].$pages[$i];
+		echo "<div class='col-md-6'>
+				<div class='panel panel-default card-view'>
+					<div class='panel-heading'>
+						<div class='pull-left'>
+							<h6 class='panel-title txt-dark'>{$lang[$y]} {$pages[$i]}</h6>
+						</div>
+						<div class='clearfix'></div>
+					</div>
+					<div class='panel-wrapper collapse in'>
+						<div class='panel-body'>
+							<div class='text'>
+								<textarea name='{$dbPlusLang}' class='tinymce'>{$settings[0]["$dbPlusLang"]}</textarea>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>";
+	}
+}
+?>
+
 	<div class="col-md-6">
 		<div class="panel panel-default card-view">
 			<div class="panel-heading">
