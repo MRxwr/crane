a371345e6cfb248fac7ccccf460c1c7e04d4db5c
MRxwr <nasserhatab@gmail.com>
2024-05-19 01:46:47 +0300

    save

--- rightSide.php
+++ rightSide.php
@@ -1,5 +1,4 @@
 <?php 
-$settings = selectDB("settings","`id` = '1'");
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
 	require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
--- template/header.php
+++ template/header.php
@@ -2,6 +2,7 @@
 require_once("admin/includes/config.php");
 require_once("admin/includes/functions.php");
 require_once("admin/includes/translate.php");
+$settings = selectDB("settings","`id` = '1'");
 ?>
 <!doctype html>
 <html lang="en">
