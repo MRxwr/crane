e09025905f1fc0d985c2c880d15add63415c3c0f
MRxwr <nasserhatab@gmail.com>
2024-05-19 00:26:33 +0300

    Update rightSide.php

--- rightSide.php
+++ rightSide.php
@@ -1,7 +1,7 @@
 <?php 
-require_once("../../admin/includes/config.php");
-require_once("../../admin/includes/functions.php");
-require_once("../../admin/includes/translate.php");
+require_once("admin/includes/config.php");
+require_once("admin/includes/functions.php");
+require_once("admin/includes/translate.php");
 
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
