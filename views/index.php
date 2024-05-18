297855dba65eaf1ac3b022aecd37d0b5728929f3
MRxwr <nasserhatab@gmail.com>
2024-05-19 00:14:49 +0300

    Update index.php

--- views/index.php
+++ views/index.php
@@ -5,8 +5,8 @@ require_once("../admin/includes/translate.php");
 
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
-	require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
+	require_once(searchFile("views","blade{$_GET["v"]}.php"));
 }else{
-	require_once("views/bladeHome.php");
+	require_once("bladeHome.php");
 }
 ?>
