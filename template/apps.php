d9b714800c7328b083dcc549e61075ce21c483ac
MRxwr <nasserhatab@gmail.com>
2024-05-19 06:03:04 +0300

    save

---
+++ template/apps.php
@@ -0,0 +1,6 @@
+<div class="col-xl-6 p-3">
+    <a href="<?php echo $socialMedia[0]["android"] ?>" target="_blank" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="fill:white;height:25px;width:25px"><path d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/></svg> Google Play</a>
+</div>
+<div class="col-xl-6 p-3">
+    <a href="<?php echo $socialMedia[0]["ios"] ?>" target="_blank" class="btn btn-outline-light w-100"><svg xmlns="http://www.w3.org/2000/svg" style="fill:white;height:25px;width:25px" viewBox="0 0 384 512"><path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z"/></svg> AppStore</a>
+</div>
